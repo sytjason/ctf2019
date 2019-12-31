043946cd1ed6a2c983cc13cb82c19aa7bd28ac7c
jason2 <r07922115@ntu.edu.tw>
2020-01-01 03:32:51 +0800

    bamboofox update

--- 0x08/note++/startdocker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run -idt -v /home/jason/Documents/ctf2019:/root/workdir --name ctf-16.04 --privileged --security-opt seccomp:unconfined ctf-16.04
-docker start ctf-16.04
-
---
+++ startdocker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker run -idt -v /home/jason/Documents/ctf2019:/root/workdir --name ctf-18.04 --privileged --security-opt seccomp:unconfined ctf-18.04
+docker start ctf-18.04
+
